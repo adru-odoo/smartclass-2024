d4629a6508f7632a39fbeccbd59fe6fdb163e875
Your Name <adru@odoo.com>
2024-08-21 20:11:59 +0000

    [ADD] new field

--- sample_module/models/spaceship.py
+++ sample_module/models/spaceship.py
@@ -9,3 +9,6 @@ class Spaceship(models.Model):
     name = fields.Char(string="name")
     manufacture_date = fields.Date(string="Date")
     number = fields.Integer(string="Number")
+
+    new_field = fields.Html(string="New field")
+    newer_field = fields.Text(string="even newer field")
